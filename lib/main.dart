762a7e630ad9ce286701ad27b6d439231538dc44
siilm <59080367+siilm@users.noreply.github.com>
2025-01-30 01:33:25 +0800

    增加了更多Flutter对隐藏导航栏的实现

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,12 @@ import 'package:code_lfa/terminal_page.dart';
 //FIXME: import 'behavior.dart';
 
 Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized(); // 必须的初始化
+  
+  // Android 全屏配置
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  
   RuntimeEnvir.initEnvirWithPackageName('com.nightmare.code');
   await initSettingStore(RuntimeEnvir.configPath);
 
@@ -33,7 +39,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorSchemeSeed: Colors.primaries[3],
       ),
-      home: TerminalPage(),
+      home: const SafeArea(
+        top: false,    // 禁用顶部安全区
+        bottom: false, // 禁用底部安全区
+        child: TerminalPage(),
+      ),
     );
   }
 }
